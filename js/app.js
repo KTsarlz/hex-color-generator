8f9882946d30495c649c4636f3312e90a237a286
Pwned <ktgonzales@tua.edu.ph>
2016-11-17 03:13:52 +0800

    Last revision for now.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-function getColor () {
-  return '#' + Math.random().toString(16).slice(2,6)
-}
 
-document.write(getColor())
+  // generate function
+  function getColor () {
+    return '#' + Math.random().toString(16).slice(2,8);
+  }
+
+  // set background color style
+  function setBackground () {
+      var bgColor = getColor();
+      document.body.style.background = bgColor;
+      document.getElementById("color-text").innerHTML = "Current color is " + bgColor;
+  }
+
+  // call function when clicked
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 32) {
+        setBackground();
+    }
+  }
